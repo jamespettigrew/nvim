c5bf9a88267763769195c9ada10e8f8870898be7
James Pettigrew <git@jamespettigrew.com>
2024-05-26 18:00:58 +1000

    feat: add confirm to buffer kill

--- nvim/lua/j/buffers.lua
+++ nvim/lua/j/buffers.lua
@@ -1,6 +1,17 @@
 local t = require('telescope.builtin')
 
-vim.keymap.set("n", "<leader>bk", "<Cmd>bdelete<CR>", { desc = "[k]ill"})
+local function kill()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local buf_windows = vim.call("win_findbuf", bufnr)
+  local modified = vim.api.nvim_get_option_value("modified", { buf = bufnr })
+  if modified and #buf_windows == 1 then
+    vim.cmd("confirm bdelete")
+  else
+    vim.cmd("bdelete")
+  end
+end
+
+vim.keymap.set("n", "<leader>bk", kill, { desc = "[k]ill", silent=true })
 vim.keymap.set("n", "<leader>bl",  t.buffers, { desc = "[l]ist"})
 vim.keymap.set("n", "<leader>bN", "<Cmd>enew<CR>", { desc = "[N]ew"})
 vim.keymap.set("n", "<leader>bn", "<Cmd>bnext<CR>", { desc = "[n]ext"})
