db607195e4143ccdc323e34222d7b49eeaf57f32
James Pettigrew <git@jamespettigrew.com>
2024-05-26 17:40:17 +1000

    refactor: window which-key descriptions

--- nvim/lua/j/windows.lua
+++ nvim/lua/j/windows.lua
@@ -1,9 +1,14 @@
 local t = require('telescope.builtin')
 
-vim.keymap.set("n", "<leader>wsn", "<Cmd>new<CR>", { desc = "[S]plit [N]ew"})
-vim.keymap.set("n", "<leader>wsf", "<Cmd>split :Telescope find_files<CR>", { desc = "[S]plit [F]ind"})
-vim.keymap.set("n", "<leader>wst", "<Cmd>split :terminal<CR>", { desc = "[S]plit [T]erminal"})
-vim.keymap.set("n", "<leader>wvn", "<Cmd>vertical :new<CR>", { desc = "[V]ertical split [N]ew"})
+require('which-key').register {
+  ['<leader>wv'] = { name = 'Split [v]ertical', _ = 'which_key_ignore' },
+  ['<leader>ws'] = { name = 'Split horizontal', _ = 'which_key_ignore' },
+}
+
+vim.keymap.set("n", "<leader>wsn", "<Cmd>new<CR>", { desc = "New"})
+vim.keymap.set("n", "<leader>wsf", "<Cmd>split :Telescope find_files<CR>", { desc = "Find"})
+vim.keymap.set("n", "<leader>wst", "<Cmd>split :terminal<CR>", { desc = "Terminal"})
+vim.keymap.set("n", "<leader>wvn", "<Cmd>vertical :new<CR>", { desc = "New"})
 vim.keymap.set(
   "n",
   "<leader>wvf",
@@ -11,9 +16,9 @@ vim.keymap.set(
     vim.api.nvim_command("vsplit")
     t.find_files()
   end,
-  { desc = "[V]ertical split [F]ind"}
+  { desc = "Find"}
 )
-vim.keymap.set("n", "<leader>wvt", "<Cmd>vertical :terminal<CR>", { desc = "[V]ertical split [T]erminal"})
+vim.keymap.set("n", "<leader>wvt", "<Cmd>vertical :terminal<CR>", { desc = "Terminal"})
 vim.keymap.set("n", "<leader>wk", "<C-w>k<CR>", { desc = "Cursor above"})
 vim.keymap.set("n", "<leader>wj", "<C-w>j<CR>", { desc = "Cursor below"})
 vim.keymap.set("n", "<leader>wh", "<C-w>h<CR>", { desc = "Cursor left"})
