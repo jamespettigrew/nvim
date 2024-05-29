f25f970290af3022db64f26e99bed89fe8418fa6
James Pettigrew <git@jamespettigrew.com>
2024-05-29 22:24:40 +1000

    feat: file 'save as'

---
+++ nvim/lua/j/files.lua
@@ -0,0 +1,52 @@
+vim.keymap.set("n", "<leader>fs",
+    function()
+        vim.lsp.buf.format()
+        vim.cmd.update()
+    end,
+    { desc = "[s]ave" }
+)
+vim.keymap.set("n", "<leader>fS",
+    function()
+        local fb = require("telescope").extensions.file_browser
+        local fb_actions = require("telescope").extensions.file_browser.actions
+        local actions = require "telescope.actions"
+        local action_state = require "telescope.actions.state"
+        local action_set = require "telescope.actions.set"
+
+        local file_created = false
+        fb.file_browser({
+            attach_mappings = function(prompt_bufnr, _)
+                fb_actions.create_from_prompt:enhance({
+                    pre = function()
+                        file_created = true
+                    end
+                })
+                action_set.edit:replace(function(_, _)
+                    actions.close(prompt_bufnr)
+
+                    local entry = action_state.get_selected_entry()
+                    if not entry or not entry.path then
+                        return
+                    end
+
+                    vim.lsp.buf.format()
+                    if file_created then
+                        vim.cmd.saveas { entry.path, bang = true }
+                        return
+                    end
+
+                    vim.ui.select({ 'Yes', 'No' }, {
+                        prompt = 'File ' .. entry.path .. ' exists; overwrite?',
+                    }, function(choice)
+                        if choice == 'Yes' then
+                            vim.cmd.saveas { entry.path, bang = true }
+                        end
+                    end)
+                end)
+
+                return true
+            end
+        })
+    end,
+    { desc = "[S]ave as" }
+)
