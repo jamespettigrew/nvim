c2bfe8de8eba8ba789831ae2f32d9ec9e8c37dc3
James Pettigrew <git@jamespettigrew.com>
2024-06-01 15:03:41 +1000

    deps: DAP plugins

---
+++ nvim/lua/plugins/dap.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"mfussenegger/nvim-dap"
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"}
+	},
+	{
+		"theHamsta/nvim-dap-virtual-text",
+		config = function()
+			require("nvim-dap-virtual-text").setup()
+		end,
+	}
+}
