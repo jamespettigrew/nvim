63bbadd3768452b377f169f04a8ceab974f7b8e8
James Pettigrew <git@jamespettigrew.com>
2024-06-10 22:03:43 +1000

    feat: dim inactive window

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -3,8 +3,16 @@ return {
   name = "catppuccin",
   priority = 1000,
   opts = {
+    dim_inactive = {
+        enabled = true, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+    },
     integrations = {
+      cmp = true,
       neogit = true,
+      nvimtree = true,
+      treesitter = true,
     },
   },
   config = function(_, opts)
