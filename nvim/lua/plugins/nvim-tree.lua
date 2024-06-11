dca0d8cf7306fd912514e417fb66e430cde9d527
James Pettigrew <github@jamespettigrew.com>
2024-06-11 21:42:05 +1000

    deps: add nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -5,7 +5,14 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
-  config = function()
-    require("nvim-tree").setup {}
+  opts = {
+      disable_netrw = true,
+      hijack_netrw = true,
+  },
+  config = function(_, opts)
+    require("nvim-tree").setup(opts)
+
+    vim.keymap.set("n", "<leader>we", "<Cmd>:NvimTreeToggle<CR>", { desc = "Toggle Explorer (nvim-tree)" })
+
   end,
 }
