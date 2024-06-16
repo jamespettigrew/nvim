9d5637f3b56815d850ea37de7381380a1e426b33
James Pettigrew <github@jamespettigrew.com>
2024-06-16 12:03:04 +1000

    feat: additional notes keymaps

--- nvim/lua/j/notes.lua
+++ nvim/lua/j/notes.lua
@@ -1,3 +1,7 @@
 vim.opt.conceallevel = 1
 
+vim.keymap.set("n", "<leader>nn", "<Cmd>ObsidianNew<CR>", { desc = "New", silent=true })
 vim.keymap.set("n", "<leader>no", "<Cmd>ObsidianQuickSwitch<CR>", { desc = "Open", silent=true })
+vim.keymap.set("n", "<leader>ni", "<Cmd>ObsidianPasteImg<CR>", { desc = "Paste Image", silent=true })
+vim.keymap.set("n", "<leader>nt", "<Cmd>ObsidianTemplate<CR>", { desc = "Insert Template", silent=true })
+vim.keymap.set("n", "<leader>ns", "<Cmd>ObsidianSearch<CR>", { desc = "Search", silent=true })
