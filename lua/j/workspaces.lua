5418b0609dfb6f094ba196d84e4c9b0d4b9686a5
James Pettigrew <github@jamespettigrew.com>
2024-12-12 15:40:47 +1100

    fix: workspace/tab desync

--- lua/j/workspaces.lua
+++ lua/j/workspaces.lua
@@ -1,23 +1,19 @@
 local M = { }
 
+M._default_path = ""
 M._workspaces = { }
 M._workspace_history = { }
 
-function M.close_current_workspace()
-  local current_workspace = table.remove(M._workspace_history)
-  if current_workspace then
-    M._workspaces[current_workspace.path] = nil
-    vim.cmd.tabclose()
+function M.active_workspace()
+  local workspace_id = M._workspace_history[#M._workspace_history]
+  return M._workspaces[workspace_id]
+end
 
-    -- Pop from history stack until we find a workspace that hasn't been closed
-    local previous_workspace = nil
-    while #M._workspace_history > 0 do
-      previous_workspace = table.remove(M._workspace_history)
-      if previous_workspace and M._workspaces[previous_workspace.path] then
-        M._focus_workspace(previous_workspace.path)
-        break
-      end
-    end
+function M.close_current_workspace()
+  local workspace_id = table.remove(M._workspace_history)
+  if workspace_id then
+    table.remove(M._workspaces, workspace_id)
+    vim.cmd.tabclose()
   end
 end
 
@@ -34,49 +30,105 @@ function M.switch_workspace()
       end,
     }, function(choice)
       if choice then
-        M._focus_workspace(choice.path)
+        M._focus_workspace(choice)
       end
   end)
 
 end
 
 function M.open_workspace(title, path)
-  if M._workspaces[path] then
-    M._focus_workspace(path)
-    return
+  for _, w in pairs(M._workspaces) do
+    if w.title == title then
+      M._focus_workspace(w)
+      return
+    end
   end
 
-  M._create_workspace(title, path)
+  vim.cmd.tabnew()
+  local w = M._create_workspace(title, path)
+  M._focus_workspace(w)
 end
 
-function M._focus_workspace(path)
-  if not M._workspaces[path] then
-    return
+function M._previous_workspace()
+  -- Pop from history stack until we find a workspace that hasn't been closed
+  while #M._workspace_history > 0 do
+    local workspace_id = M._workspace_history[#M._workspace_history]
+    if M._workspaces[workspace_id] then
+      return M._workspaces[workspace_id]
+    end
+
+    table.remove(M._workspace_history)
   end
 
-  local workspace = M._workspaces[path]
-  vim.api.nvim_set_current_dir(path)
-  vim.api.nvim_set_current_tabpage(workspace.tabpage)
-  table.insert(M._workspace_history, workspace)
+  return nil
+end
+
+function M._focus_workspace(w)
+  if w.path ~= "" then
+    vim.api.nvim_set_current_dir(w.path)
+  else
+    vim.api.nvim_set_current_dir(M._default_path)
+  end
+
+  vim.api.nvim_set_current_tabpage(w.tabpage)
+  table.insert(M._workspace_history, w.id)
 end
 
 function M._create_workspace(title, path)
-  vim.api.nvim_command("$tabnew")
   vim.api.nvim_command("Tabby rename_tab " .. title)
   local tabpage = vim.api.nvim_get_current_tabpage()
-  local new_workspace = {
+  local id = tabpage
+
+  local w = {
+    id = id,
     title = title,
     path = path,
     tabpage = tabpage,
   }
-  M._workspaces[path] = new_workspace
-  M._focus_workspace(path)
+  M._workspaces[id] = w
+  vim.api.nvim_tabpage_set_var(tabpage, "workspace_id", id)
 
-  require("telescope.builtin").find_files{}
+  return w
+end
+
+function M._tab_entered()
+  local tabpage = vim.api.nvim_get_current_tabpage()
+  local ok, workspace_id = pcall(vim.api.nvim_tabpage_get_var, tabpage, "workspace_id")
+  if not ok then
+    return
+  end
+
+  M._focus_workspace(M._workspaces[workspace_id])
+end
+
+function M._tab_closed()
+    local tabpages = {}
+    for _, tabpage in pairs(vim.api.nvim_list_tabpages()) do
+      tabpages[tabpage] = true
+    end
+
+    local orphan_workspaces = {}
+    for workspace_id, w in pairs(M._workspaces) do
+      if not tabpages[w.tabpage] then
+        orphan_workspaces[workspace_id] = workspace_id
+      end
+    end
+
+    for workspace_id in pairs(orphan_workspaces) do
+      table.remove(M._workspaces, workspace_id)
+    end
+
+    local prev = M._previous_workspace()
+    if prev then
+      M._focus_workspace(prev)
+    end
 end
 
 function M.setup()
-  vim.o.showtabline = 2 -- Always display tabs, even if only one open
+  M._default_path = vim.fn.getcwd()
+  -- Create default workspace
+  local default_workspace = M._create_workspace("default", "")
+  M._focus_workspace(default_workspace)
 
   vim.keymap.set("n", "<leader><TAB>k", function() M.close_current_workspace() end, { desc = "Close this workspace" })
   vim.keymap.set("n", "<leader><TAB>o",
@@ -85,6 +137,9 @@ function M.setup()
     end,
     { desc = "Open project" })
   vim.keymap.set("n", "<leader><TAB><TAB>", function() M.switch_workspace() end, { desc = "Switch workspace"  })
+
+  vim.api.nvim_create_autocmd("TabEnter", { callback=M._tab_entered})
+  vim.api.nvim_create_autocmd("TabClosed", { callback=M._tab_closed})
 end
 
 return M
--- lua/plugins/tabby.lua
+++ lua/plugins/tabby.lua
@@ -12,24 +12,29 @@ return {
       win = 'TabLine',
       tail = 'TabLine',
     }
+    vim.o.showtabline = 2 -- Always display tabs, even if only one open
     require('tabby').setup({
       line = function(line)
+        local active_workspace_element = {}
+        local hl = theme.current_tab or theme.tab
+
+        local active_workspace = require("j.workspaces").active_workspace()
+        if active_workspace then
+          active_workspace_element = {
+            line.sep('', hl, theme.fill),
+            active_workspace.title,
+            line.sep('', hl, theme.fill),
+            margin = ' ',
+            hl = hl,
+          }
+        end
+
         return {
           {
             { '  ', hl = theme.head },
             line.sep('', theme.head, theme.fill),
           },
-          line.tabs().foreach(function(tab)
-            local hl = tab.is_current() and theme.current_tab or theme.tab
-            return {
-              line.sep('', hl, theme.fill),
-              tab.number(),
-              tab.name(),
-              line.sep('', hl, theme.fill),
-              hl = hl,
-              margin = ' ',
-            }
-          end),
+          active_workspace_element,
           hl = theme.fill,
         }
       end,
