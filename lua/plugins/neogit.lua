29dbf205e7b714c379e796456c99ede43c9fa233
James Pettigrew <git@jamespettigrew.com>
2025-09-12 11:44:33 +0100

    feat: remove all "tab" defaults from Neogit
    Our workspaces functionality relies on authority over tabs,
    and having Neogit create new tabs breaks workspaces. I also
    prefer having splits and floating panels for much of this
    functionality.

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -6,8 +6,48 @@ return {
 
     "nvim-telescope/telescope.nvim", -- optional
   },
+  opts = {
+    kind = "replace",
+    commit_editor = {
+      kind = "floating",
+      show_staged_diff = true,
+      staged_diff_split_kind = "split",
+      spell_check = true,
+    },
+    commit_select_view = {
+      kind = "floating",
+    },
+    commit_view = {
+      kind = "floating",
+    },
+    log_view = {
+      kind = "floating",
+    },
+    rebase_editor = {
+      kind = "floating",
+    },
+    reflog_view = {
+      kind = "floating",
+    },
+    merge_editor = {
+      kind = "floating",
+    },
+    preview_buffer = {
+      kind = "floating_console",
+    },
+    popup = {
+      kind = "floating_console",
+    },
+    stash = {
+      kind = "floating_console",
+    },
+    refs_view = {
+      kind = "floating",
+    },
+  },
   config = function(_, opts)
     require("neogit").setup(opts)
-    vim.keymap.set("n", "<leader>gs", "<Cmd>:Neogit kind=replace<CR>", { desc = "[S]tatus"})
+
+    vim.keymap.set("n", "<leader>gs", "<Cmd>:Neogit kind=vsplit<CR>", { desc = "[S]tatus"})
   end
 }
