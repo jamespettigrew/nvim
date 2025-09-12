46a4b041133bc1624ace0602d5e66575b9d5ae7e
James Pettigrew <git@jamespettigrew.com>
2025-09-12 13:47:56 +0100

    fix: duplicate LspAttach config

--- init.lua
+++ init.lua
@@ -169,6 +169,22 @@ require('lazy').setup({
           --  the definition of its *type*, not where it was *defined*.
           map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
 
+          -- See `:help K` for why this keymap
+          map('K', vim.lsp.buf.hover, 'Hover Documentation')
+          map('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+          -- Create a command `:Format` local to the LSP buffer
+          vim.api.nvim_buf_create_user_command(event.buf, 'Format', function(_)
+            vim.lsp.buf.format()
+          end, { desc = 'Format current buffer with LSP' })
+
+          vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+            pattern = "*.wgsl",
+            callback = function()
+              vim.bo.filetype = "wgsl"
+            end,
+          })
+
           -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
           ---@param client vim.lsp.Client
           ---@param method vim.lsp.protocol.Method
@@ -838,55 +854,6 @@ vim.defer_fn(function()
     },
   }
 end, 0)
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(event)
-    -- NOTE: Remember that lua is a real programming language, and as such it is possible
-    -- to define small helper and utility functions so you don't have to repeat yourself
-    -- many times.
-    --
-    -- In this case, we create a function that lets us more easily define mappings specific
-    -- for LSP related items. It sets the mode, buffer and description for us each time.
-    local nmap = function(keys, func, desc, mode)
-      mode = mode or 'n'
-      vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-    end
-
-    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-    nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-    nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-    nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-    nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-    nmap('<leader><TAB>s', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-    -- See `:help K` for why this keymap
-    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-    nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-    -- Lesser used LSP functionality
-    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-    nmap('<leader><TAB>a', vim.lsp.buf.add_workspace_folder, '[A]dd Folder')
-    nmap('<leader><TAB>r', vim.lsp.buf.remove_workspace_folder, '[R]emove Folder')
-    nmap('<leader><TAB>l', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, '[L]ist Folders')
-
-    -- Create a command `:Format` local to the LSP buffer
-    vim.api.nvim_buf_create_user_command(event.buf, 'Format', function(_)
-      vim.lsp.buf.format()
-    end, { desc = 'Format current buffer with LSP' })
-
-    vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-      pattern = "*.wgsl",
-      callback = function()
-        vim.bo.filetype = "wgsl"
-      end,
-    })
-  end,
-})
 
 -- document existing key chains
 require('which-key').register {
